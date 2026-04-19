833dbb72bd9e2eba9e299f22570725cf7516169f
Kyiotaro <kyiotaro7@gmail.com>
2026-04-19 17:47:23 +0700

    Display raw API response in ToDoList

--- flutter_modul_6/lib/pages/ToDo_list.dart
+++ flutter_modul_6/lib/pages/ToDo_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modul_6/service/http_service.dart';
 
 class ToDoList extends StatefulWidget {
   const ToDoList({super.key});
@@ -8,14 +9,29 @@ class ToDoList extends StatefulWidget {
 }
 
 class _ToDoListState extends State<ToDoList> {
+  String result = "";
+  late HttpService service;
+
+  @override
+  void initState() {
+    service = HttpService();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    service.getPopularMovies()?.then((value) {
+      setState(() {
+        result = value.toString();
+      });
+    });
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('To Do List'),
+        title: const Text("Popular Movies"),
       ),
-      body: const Center(
-        child: Text('To Do List Content'),
+      body: Container(
+        child: Text(result),
       ),
     );
   }
