8e7edab349c4a733bdd0077e811c394af242f04d
Kyiotaro <kyiotaro7@gmail.com>
2026-04-19 17:48:14 +0700

    Update HttpService to return ToDo objects

--- flutter_modul_6/lib/service/http_service.dart
+++ flutter_modul_6/lib/service/http_service.dart
@@ -1,20 +1,21 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:flutter_modul_6/models/ToDo.dart';
 
 class HttpService {
   final String baseUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-  Future<List?> getPopularMovies() async {
+  Future<List<ToDo>?> getPopularMovies() async {
     final String uri = baseUrl;
 
     http.Response result = await http.get(Uri.parse(uri));
 
     if (result.statusCode == HttpStatus.ok) {
       print("Success");
-      final jsonResponse = json.decode(result.body);
-      // For now, returning as list of dynamic
-      return jsonResponse;
+      final List jsonResponse = json.decode(result.body);
+      List<ToDo> todos = jsonResponse.map((i) => ToDo.fromJson(i)).toList();
+      return todos;
     } else {
       print("Fail");
       return null;
